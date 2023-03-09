a643fc0b16a215a3c0a739dabf5055eceb85f3f3
Dakota Greenlee <dg2120032@gmail.com>
2023-03-08 19:15:44 -0800

    env

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const sequelize = new Sequelize(process.env.PG_URI)
 
 try {
     sequelize.authenticate()
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
 } catch (err) {
     console.log(`Unable to connect to PG: ${err}`)
 }
